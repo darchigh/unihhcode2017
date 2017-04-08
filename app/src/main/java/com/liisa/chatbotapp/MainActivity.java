745e03264660d5726461859b8000f2bac9924958
6vaht <6vaht@informatik.uni-hamburg.de>
2017-04-08 16:31:45 +0200

    redo

--- app/src/main/java/com/liisa/chatbotapp/MainActivity.java
+++ app/src/main/java/com/liisa/chatbotapp/MainActivity.java
@@ -1,13 +1,35 @@
 package com.liisa.chatbotapp;
 
+import android.content.Context;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private Context mContext;
+    private String workspace_id;
+    private String conversation_username;
+    private String conversation_password;
+    private EditText inputMessage;
+    private ImageButton btnSend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContext = getApplicationContext();
+        conversation_username = mContext.getString(R.string.conversation_username);
+        conversation_password = mContext.getString(R.string.conversation_password);
+        workspace_id = mContext.getString(R.string.workspace_id);
+
+        inputMessage = (EditText) findViewById(R.id.message);
+        btnSend = (ImageButton) findViewById(R.id.sendButton);
     }
 }
+
