b58bf036c4861bc37398b8521cdaed94ada506df
6vaht <6vaht@informatik.uni-hamburg.de>
2017-04-09 10:22:55 +0200

    imports

--- app/src/main/java/com/liisa/chatbotapp/MainActivity.java
+++ app/src/main/java/com/liisa/chatbotapp/MainActivity.java
@@ -2,7 +2,7 @@ package com.liisa.chatbotapp;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.os.Message;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,6 +13,9 @@ import com.ibm.watson.developer_cloud.conversation.v1.ConversationService;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageRequest;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
 import com.ibm.mobilefirstplatform.clientsdk.android.core.api.BMSClient;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
+import android.support.v7.widget.RecyclerView;
 
 import com.chatbotapp.MambaWebApi;
 import com.chatbotapp.mambaObj.SearchResult;
@@ -101,7 +104,6 @@ public class MainActivity extends AppCompatActivity {
                             }
                             messageArrayList.add(outMessage);
                         }
-
                         runOnUiThread(new Runnable() {
                             public void run() {
                                 mAdapter.notifyDataSetChanged();
