f2b5d2b4f0895825fa6ce37c964524d472dfa6da
6vaht <6vaht@informatik.uni-hamburg.de>
2017-05-14 12:26:34 +0200

    statisticsactvitiy

---
+++ app/src/main/java/com/liisa/chatbotapp/StatisticsActivity.java
@@ -0,0 +1,17 @@
+package com.liisa.chatbotapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by liisa_000 on 14/05/2017.
+ */
+
+public class StatisticsActivity  extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.statistics_activity);
+    }
+}
