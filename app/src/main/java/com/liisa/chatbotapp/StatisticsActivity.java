2457dab7b9ff42a002a1013fa55cf76916c5591f
6vaht <6vaht@informatik.uni-hamburg.de>
2017-08-12 15:45:13 +0200

    Merge branch 'master' of C:\Users\liisa_000\AndroidStudioProjects\ChatbotApp with conflicts.

--- app/src/main/java/com/liisa/chatbotapp/StatisticsActivity.java
+++ app/src/main/java/com/liisa/chatbotapp/StatisticsActivity.java
@@ -1,46 +1,60 @@
 package com.liisa.chatbotapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ListView;
 
 /**
  * Created by liisa_000 on 14/05/2017.
  */
 
-public class StatisticsActivity  extends AppCompatActivity {
-    private PieChart mChart;
+public class StatisticsActivity extends AppCompatActivity {
+
+    ImageButton BarChart1;
+    ImageButton BarChart2;
+    ImageButton BarChart3;
+    ImageButton BarChart4;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.statistics_activity);
-        mChart = (PieChart) findViewById(R.id.PieChart);
+        BarChart1 = (ImageButton) findViewById(R.id.Barchart1);
+        BarChart2 = (ImageButton) findViewById(R.id.Barchart2);
+        BarChart3 = (ImageButton) findViewById(R.id.Barchart3);
+        BarChart4 = (ImageButton) findViewById(R.id.Barchart4);
 
-
-        ArrayList<PieEntry> entries = new ArrayList<PieEntry>();
-        float range = (float) 2.2;
-        entries.add(1,new PieEntry(range, "la"));
-        entries.add(2,new PieEntry(range, "la"));
-        entries.add(3,new PieEntry(range, "la"));
-
-        PieDataSet dataSet = new PieDataSet(entries, "introduction");
-
-        PieData data = new PieData(dataSet);
-
-
-
-
-    }
-
-
-}
+        BarChart1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(StatisticsActivity.this, BarChartActivity.class);
+                StatisticsActivity.this.startActivity(myIntent);
+            }
+        });
+        BarChart2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(StatisticsActivity.this, BarChartActivity.class);
+                StatisticsActivity.this.startActivity(myIntent);
+            }
+        });
+        BarChart3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(StatisticsActivity.this, BarChartActivity.class);
+                StatisticsActivity.this.startActivity(myIntent);
+            }
+        });
+        BarChart4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(StatisticsActivity.this, BarChartActivity.class);
+                StatisticsActivity.this.startActivity(myIntent);
+            }
+        });
+    } }
